d024a228644697aae490b45ce1e4d48a20e35e97
Benjamin Schubert <contact@benschubert.me>
2020-06-08 19:46:45 +0100

    Add install script and remove gitignore
    This is now meant to be checked out somewhere else then 'installed'
    locally

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CWD=$(realpath $(dirname "$0"))
+
+mkdir -p ~/.config/git
+
+for file in \
+        .bashrc \
+        .tmux.conf \
+        .tmux-status.sh \
+        .vimrc \
+        .config/git/config \
+        .config/git/ignore\
+; do
+    ln -fs "${CWD}/${file}" "${HOME}/${file}"
+done
